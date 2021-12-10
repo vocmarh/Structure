2804811070a8f9ea61d02c2ffb6e45be717fb917
s.hramcov <s.hramcov@HRAMCOV>
2021-12-10 16:15:11 +0500

    Добавьте файлы проекта.

---
+++ Structure/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Structure
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Equation equation = new Equation();
+            equation.Root();            
+            Console.ReadKey();
+        }
+    }
+    struct Equation
+    {
+        public double k;
+        public double b;
+        public double x;
+
+                
+
+        public void Root()
+        {
+            Console.WriteLine("Решите следующее уравнение: 0 = kx + b. Найдите значение x");
+            Console.Write("Введите значение b: ");
+            b = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Введите значение k: ");
+            k = Convert.ToDouble(Console.ReadLine());
+            if (k == 0)
+            {
+                Console.WriteLine("При данном значении решение уровнения невозможно. Делить на ноль нельзя!");
+            }
+            x = (0 - b) / k;
+            Console.WriteLine(x);
+                        
+        }
+
+    }
+}
